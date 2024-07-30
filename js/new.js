fe6fc44b88e1a9dce9651d70697f4e407f660517
dasheng66688 <139194398+dasheng66688@users.noreply.github.com>
2024-07-30 19:13:25 +0800

    Add files via upload

---
+++ js/new.js
@@ -0,0 +1,6 @@
+﻿
+if (window.console&&window.console.log){  
+	console.log("%c更多免费资源访问27CaT论坛：Www.27CaT.CoM - 酒入论坛：Www.Fzb2.ToP 和 Www.2Fzb.BiZ 访问不了挂梯子即可。","color:#19a334");
+	console.log("%c更多免费资源访问27CaT论坛：Www.27CaT.CoM - 酒入论坛：Www.Fzb2.ToP 和 Www.2Fzb.BiZ 访问不了挂梯子即可。","color:#19a334");
+}  
+
